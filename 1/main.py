c76008f3e0c87c2a0a9e600a12710d7f5e10e7a6
miho19 <miho19@hotmail.co.nz>
2021-06-18 13:54:13 +1200

    fixed edge case of daily 1 and added asserts for tests

--- 1/main.py
+++ 1/main.py
@@ -12,27 +12,45 @@ def bin_search(arr, low, high, q, target):
 
     
     if(high < low):
-        return -1    
+        return -1
+       
     
     if((target - q) == arr[mid]):
-        
-        return arr[mid]
-    if((target - q) > arr[mid]):
+        return mid
+    
+    if(arr[mid] + q > target):
         return bin_search(arr, low, mid - 1, q, target)
     else:
         return bin_search(arr, mid + 1, high, q, target)
 
 
 def sum_target(arr, target):
+    l1 = sorted(arr)
+
     if len(arr) > 2:
-        l1 = sorted(arr)
         for i in range(len(arr)):
             print
             b = bin_search(l1, i, len(arr) -1, l1[i], target)
             if b > -1:
-                print(target, " = " , b , " + ", l1[i])
-                break
+                return [l1[i], l1[b]]
+        return [0]
+    else:
+        if(len(l1) == 2):
+            if(l1[0] + l1[1] == target):
+                return [l1[0], l1[1]]
+            else:
+                return [0]
+        else:
+            return [0]
+
+
  
 
-
-sum_target([10, 15, 3, 7], 10)
+assert(sum(sum_target([10, 15, 3, 7], 10)) == 10)
+assert(sum(sum_target([10, 15, 3, 7], 17)) == 17)
+assert(sum(sum_target([10, 15, 3, 7], 99)) == 0)
+assert(sum(sum_target([3, 7], 99)) == 0)
+assert(sum(sum_target([3, 7], 10)) == 10)
+assert(sum(sum_target([7], 10)) == 0)
+assert(sum(sum_target([10, 15, 3, 7], 18)) == 18)
+assert(sum(sum_target([10, 15, 3, 7], 25)) == 25)
